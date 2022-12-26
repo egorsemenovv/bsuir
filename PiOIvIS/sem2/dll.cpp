fccc4ae6ff11e82c3fd43f617db1330d1de81833
Semen Egorov <brose2004@mail.ru>
2022-12-26 21:44:07 +0300

    added methods push_front, print, print_back, also rewrited method push_back

--- PiOIvIS/sem2/dll.cpp
+++ PiOIvIS/sem2/dll.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 template <typename Type>
 
 class DoublyLinkedList {
@@ -6,8 +9,8 @@ private:
 
 	struct Node {
 		Type item;
-		Node* prev;
-		Node* next;
+		Node* prev = nullptr;
+		Node* next = nullptr;
 	};
 
 	Node* first;
@@ -18,15 +21,30 @@ private:
 public:
 
 	DoublyLinkedList();
-
+	DoublyLinkedList(Type);
 	~DoublyLinkedList();
 
 	void push_back(Type);
+	void push_front(Type);
+
+	void print();
+	void print_back();
+
 };
 
 template <typename Type>
 DoublyLinkedList<Type>::DoublyLinkedList() {
+	first = nullptr;
+	last = nullptr;
+}
 
+template <typename Type>
+DoublyLinkedList<Type>::DoublyLinkedList(Type item)
+{
+	first = last = new DoublyLinkedList<Type>::Node;
+	first->prev = nullptr;
+	first->next = nullptr;
+	first->item = item;
 }
 
 template <typename Type>
@@ -40,13 +58,60 @@ bool DoublyLinkedList<Type>::is_empty() {
 }
 
 template <typename Type>
-void DoublyLinkedList<Type>::push_back(Type _item) {
-	Node* temp = new Node(_item);
+void DoublyLinkedList<Type>::push_back(Type item) {
+	DoublyLinkedList<Type>::Node* temp = new DoublyLinkedList<Type>::Node;
+	temp->item = item;
 	if (is_empty()) {
-		first = temp;
-		last = temp;
+		first = last = temp;
 		return;
 	}
+	temp->prev = last;
 	last->next = temp;
 	last = temp;
+}
+
+template <typename Type>
+void DoublyLinkedList<Type>::push_front(Type item) {
+	DoublyLinkedList<Type>::Node* temp = new DoublyLinkedList<Type>::Node;
+	temp->item = item;
+	if (is_empty()) {
+		first = last = temp;
+		return;
+	}
+	temp->next = first;
+	first->prev = temp;
+	first = temp;
+}
+
+template <typename Type>
+void DoublyLinkedList<Type>::print() {
+	DoublyLinkedList<Type>::Node* temp = first;
+	if (is_empty()) return;
+	while (temp) {
+		cout << temp->item << ' ';
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+template <typename Type>
+void DoublyLinkedList<Type>::print_back() {
+	DoublyLinkedList<Type>::Node* temp = last;
+	if (is_empty()) return;
+	while (temp) {
+		cout << temp->item << ' ';
+		temp = temp->prev;
+	}
+	cout << endl;
+}
+
+int main() {
+	DoublyLinkedList<int> a;
+	a.push_back(3);
+	a.push_front(2);
+	a.push_back(4);
+	a.push_front(1);
+	a.print();
+	a.print_back();
+	return 0;
 }
