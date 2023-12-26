d381ebfef2267c46b535e0c8ebbac6a14ad49698
Semen Egorov <brose2004@mail.ru>
2023-12-26 17:54:36 +0300

    Relief of code duplication, also radix for LSD Radix sort now is constant value

--- PPOIS/Sorts/src/main/java/org/example/Sort.java
+++ PPOIS/Sorts/src/main/java/org/example/Sort.java
@@ -6,6 +6,9 @@ import org.example.exceptions.ElementIsNullException;
 import java.util.*;
 
 public class Sort {
+
+    private static final int RADIX = 10;
+
     /**
      * "Shell" sort for array
      * @param array input array to be sorted
@@ -14,33 +17,12 @@ public class Sort {
      * @throws ContainerIsNullException if container is empty or null
      */
     public static <T extends Comparable<T>> void shellSort(T[] array) throws ElementIsNullException, ContainerIsNullException {
-
-        if(array == null || array.length == 0){
+        if(array == null){
             throw new ContainerIsNullException("Array is null");
         }
-
-        int d = 1;
-
-        while (d <= array.length / 3) {
-            d = d * 3 + 1;
-        }
-
-        while (d > 0) {
-            for (int outer = d; outer < array.length; outer++) {
-                T temp = array[outer];
-                int inner = outer;
-                if(array[inner-d] == null)
-                    throw new ElementIsNullException("Element with index " + (inner-d) + " is null");
-                while (inner > d - 1 && array[inner - d].compareTo(temp) > 0) {
-                    array[inner] = array[inner - d];
-                    inner -= d;
-                }
-
-                array[inner] = temp;
-            }
-
-            d = (d - 1) / 3;
-        }
+        List<T> convertedArrayToList = arrayToListConverter(array);
+        shellSort(convertedArrayToList);
+        listToArrayConverter(array, convertedArrayToList);
     }
 
     /**
@@ -52,7 +34,7 @@ public class Sort {
      */
     public static <T extends Comparable<T>> void shellSort(List<T> list) throws ElementIsNullException, ContainerIsNullException{
 
-        if(list == null || list.isEmpty()){
+        if(list == null){
             throw new ContainerIsNullException("List is null");
         }
 
@@ -72,7 +54,6 @@ public class Sort {
                     list.set(inner, list.get(inner - d));
                     inner -= d;
                 }
-
                 list.set(inner, temp);
             }
             d = (d - 1) / 3;
@@ -86,40 +67,13 @@ public class Sort {
      * @throws ElementIsNullException if one or more elements is null
      * @throws ContainerIsNullException if container is empty or null
      */
-    public static <T extends Number> void lsdRadixSort(T[] array) throws ElementIsNullException, ContainerIsNullException
-    {
-        if(array == null || array.length == 0){
+    public static <T extends Number> void lsdRadixSort(T[] array) throws ElementIsNullException, ContainerIsNullException {
+        if(array == null){
             throw new ContainerIsNullException("Array is null");
         }
-
-        Queue<T>[] buckets = new Queue[10];
-        for (int i = 0; i < 10; i++)
-            buckets[i] = new LinkedList<T>();
-
-        boolean sorted = false;
-        int expo = 1;
-
-        while (!sorted) {
-            sorted = true;
-
-            for (T item : array) {
-                if(item == null){
-                    throw new ElementIsNullException("Element is null");
-                }
-                int bucket = (item.intValue() / expo) % 10;
-                if (bucket > 0) {
-                    sorted = false;
-                }
-                buckets[bucket].add(item);
-            }
-
-            expo *= 10;
-            int index = 0;
-
-            for (Queue<T> bucket : buckets)
-                while (!bucket.isEmpty())
-                    array[index++] = bucket.remove();
-        }
+        List<T> convertedArrayToList = arrayToListConverter(array);
+        lsdRadixSort(convertedArrayToList);
+        listToArrayConverter(array, convertedArrayToList);
     }
 
     /**
@@ -131,13 +85,12 @@ public class Sort {
      */
     public static <T extends Number> void lsdRadixSort(List<T> list) throws ElementIsNullException, ContainerIsNullException
     {
-        if(list == null || list.isEmpty()){
+        if(list == null){
             throw new ContainerIsNullException("List is null");
         }
-
-        Queue<T>[] buckets = new Queue[10];
-        for (int i = 0; i < 10; i++)
-            buckets[i] = new LinkedList<T>();
+        Queue<T>[] buckets = new Queue[RADIX];
+        for (int i = 0; i < RADIX; i++)
+            buckets[i] = new LinkedList<>();
 
         boolean sorted = false;
         int expo = 1;
@@ -149,19 +102,41 @@ public class Sort {
                 if(item == null){
                     throw new ElementIsNullException("Element is null");
                 }
-                int bucket = (item.intValue() / expo) % 10;
+                int bucket = (item.intValue() / expo) % RADIX;
                 if (bucket > 0) {
                     sorted = false;
                 }
                 buckets[bucket].add(item);
             }
 
-            expo *= 10;
+            expo *= RADIX;
             int index = 0;
 
             for (Queue<T> bucket : buckets)
                 while (!bucket.isEmpty())
                     list.set(index++, bucket.remove());
+        }
+    }
+
+    /**
+     * converts array to list
+     * @param array massive to be represented as list
+     * @return list
+     * @param <T> Type of array
+     */
+    private static <T> List<T> arrayToListConverter(T[] array){
+        return new ArrayList<>(Arrays.asList(array));
+    }
+
+    /**
+     * rewrites array`s data from list
+     * @param array to be rewritten from list
+     * @param list from which data is copied
+     * @param <T> Type of list
+     */
+    private static <T> void listToArrayConverter(T[] array, List<T> list){
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
         }
     }
 }
